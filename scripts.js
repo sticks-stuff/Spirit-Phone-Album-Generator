233eddccc84d1763e56233e6c9b2e36a071e57a8
Thestickman391 <stickman391@gmail.com>
2020-10-11 14:56:11 +1300

    Draw font

--- scripts.js
+++ scripts.js
@@ -3,8 +3,15 @@ window.onload = function() {
 	var canvas = document.getElementById("albumBackground");
 	var ctx = canvas.getContext("2d");
 	var img = new Image();
+	var f = new FontFace('SpiritPhone', 'url(SpiritPhone.ttf)');
 	img.onload = function() {
 		ctx.drawImage(img, 0, 0);
+		f.load().then(function() {
+			ctx.font = "30px SpiritPhone";
+			ctx.fillStyle = "white";
+			ctx.textAlign = "center";
+			ctx.fillText("Hello World", canvas.width/2, canvas.height/2); 
+		});
 	}
 	img.src = 'spiritPhoneBackground.png';
 }
